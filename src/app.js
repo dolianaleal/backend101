57c3c3966add9a05480ce13d1d370d60f955e19d
dolianaleal <dolianalealrivas@gmail.com>
2024-12-22 15:54:25 -0300

    webservices_2

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from 'express'
 const PORT = 8080
 const app = express() //Cuando llame a app, se ejecuta Express
 
+app.use(express.urlencoded({extended:true})) // Permite realizar peticiones mas complejas (req.query)
+
 const products = [
     {
         id: 1,
@@ -14,7 +16,7 @@ const products = [
     {
         id: 2,
         nombre: "Product 2",
-        marca: "Test 2",
+        marca: "Test 1",
         precio: 2000,
         stock: 10
     },
@@ -38,11 +40,24 @@ app get('/saludar', (req, res) => {
     res.send("Hola")
 })
 
+app.get ('/products', (req, res) => {
+    let {marca, precio} = req.query
+    let prodsFiltrados = products.filter(prod => prod.marca.toLowerCase === marca)
+    res.send(prodsFiltrados)
+})
+
+
 // : hace referencia a un parametro
 app.get('/products/:pid', (req, res) => {
     const productId = parseInt(req.params.pid)// Toda informacion que venga por el parametro, es un string
     
     const producto = products.find(prod => prod.id === + productId)
+
+    if (producto) {
+        res.send(producto)
+    } else {
+        res.send("El producto no existe")
+    }
 
     res.send(`Producto con ID: ${productId}`)
 })
