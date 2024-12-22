b573a72afaed090d37f8195bdcd941bc18c93090
dolianaleal <dolianalealrivas@gmail.com>
2024-12-22 13:58:37 -0300

    creting server_1

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const PORT = 8080
 const server = http.createServer((req, res) => {
     res.end('Hola, 1st server')
 })
+server.listen(PORT, 'localhost', () => {
+    console.log(`Server on port ${PORT}`);
+})
