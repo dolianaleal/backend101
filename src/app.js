355922950a3da9f9bf18482aae17fcb04105b5c6
dolianaleal <dolianalealrivas@gmail.com>
2024-12-27 07:07:55 -0300

    postman_1

--- src/app.js
+++ src/app.js
@@ -1,37 +1,15 @@
+import exp from 'constants'
 import express from 'express'
+import crypto from 'crypto'
 
 const PORT = 8080
 const app = express() //Cuando llame a app, se ejecuta Express
 
+app.use(express.json()) //Permite enviar y recibir JSON en las peticiones
 //filtro por la marca enviada en los queries
-app.use(express.urlencoded({extended:true})) // Permite realizar peticiones mas complejas (req.query)
+app.use(express.urlencoded({ extended: true })) // Permite realizar peticiones mas complejas (req.query)
 
-const products = [
-    {
-        id: 1,
-        nombre: "Product 1",
-        marca: "Test 1",
-        precio: 1200,
-        stock: 30
-    },
-    {
-        id: 2,
-        nombre: "Product 2",
-        marca: "Test 1",
-        precio: 2000,
-        stock: 10
-    },
-    {
-        id: 2,
-        nombre: "Product 3",
-        marca: "Test 3",
-        precio: 1200,
-        stock: 20
-    }
-
-]
-
-
+const products = []
 
 app.get('/', (req, res) => {
     res.send("Hi, server")
@@ -41,23 +19,18 @@ app get('/saludar', (req, res) => {
     res.send("Hola")
 })
 
-app.get ('/products', (req, res) => {
-    let {marca, precio} = req.query
+app.get('/products', (req, res) => {
+    let { marca, precio } = req.query
     //Filtro por la marca enviada en los queries
     let prodsFiltrados = products.filter(prod => prod.marca.toLowerCase === marca)
 
     res.status(200).send(prodsFiltrados)
 })
 
-app.post('/products', (req, res) => {
-    let {nombre,marca,precio,stock} = req.body
-})
-
-
 // : hace referencia a un parametro
 app.get('/products/:pid', (req, res) => {
     const productId = parseInt(req.params.pid)// Toda informacion que venga por el parametro, es un string
-    
+
     const producto = products.find(prod => prod.id === + productId)
 
     if (producto) {
@@ -67,6 +40,20 @@ app.get('/products/:pid', (req, res) => {
     }
 
     res.send(`Producto con ID: ${productId}`)
+})
+
+app.post('/products', (req, res) => {
+    let { nombre, marca, precio, stock } = req.body
+
+    const newProduct= {
+        id: crypto.randomBytes(10).toString('hex')
+        nombre: nombre,
+        marca: marca,
+        precio: precio,
+        stock: stock
+    }
+    products.push(newProduct)
+    res.status(201).send(`Producto creado con el id: ${newProduct.id}`)
 })
 
 app.listen(PORT, () => {
